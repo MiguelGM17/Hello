df3048f301ebe05fa229d4c410292949987941a7
MiguelGM17 <60486996+MiguelGM17@users.noreply.github.com>
2020-03-12 15:32:15 -0600

    Update Ejemplo For-Fibonacci.c

--- Ejemplo For-Fibonacci.c
+++ Ejemplo For-Fibonacci.c
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 int main(void)
 {
-  int x,y,z,cont;
+  int x,y,z,cont,paro;
   x=0;
   y=1;
-  for (cont=0;cont<20;cont++)
-  {
+  printf ("Cuantos numeros quieres?");
+  scanf ("%d",&paro);
+  
+  if (paro==1){
+  	printf ("%d\n",x);
+  }
+  else if (paro==2){
+  	printf("%d\n%d\n",x,y);
+  }
+  else{
+		printf("%d\n%d\n",x,y);
+ 	 for (cont=0;cont<paro-2;cont++)
+  	{
       z=x+y;
       printf("%d\n",z);
       x=y;
       y=z;
+ 	 }  
   }
   getchar();
   return 0;
